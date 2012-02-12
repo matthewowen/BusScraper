cf76ec1ae65362b5942042450b87c20982bab31c
Matthew Owen <matthewo@aroon.local>
2012-02-12 13:59:18 +0000

    update docs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='BusScraper',
-    version='0.2.1',
+    version='0.2.2',
     author='Matthew Owen',
     author_email='matthew.owen95@gmail.com',
     py_modules=['busscraper'],
